6bfd2334a7a96538ff47f1bf772c99669d3f46f8
wuyufei <1328988774@qq.com>
2023-09-04 17:20:18 +0800

    compelete the lab1

--- lab/lab1/backchain.py
+++ lab/lab1/backchain.py
@@ -15,8 +15,30 @@ from zookeeper import ZOOKEEPER_RULES
 
 
 def backchain_to_goal_tree(rules, hypothesis):
-    raise NotImplementedError
+    results = [hypothesis]
+    for rule in rules:
+        expr = rule.consequent()[0]
+        bindings = match(expr, hypothesis)
+        if bindings or expr == hypothesis:          # there is no variables in hypothesis in TEST 14(data in tests.py)
+            antecedent = rule.antecedent()
+            if isinstance(antecedent, AND):
+                new_results = []
+                for item in populate(rule.antecedent(), bindings):
+                    new_results.append(backchain_to_goal_tree(rules, item))
+                results.append(AND(new_results))
+            elif isinstance(antecedent, OR):
+                new_results = []
+                for item in populate(rule.antecedent(), bindings):
+                    new_results.append(backchain_to_goal_tree(rules, item))
+                results.append(OR(new_results))
+            else:
+                new_hypothesis = populate(antecedent, bindings)
+                results.append(backchain_to_goal_tree(rules, new_hypothesis))
+                results.append(new_hypothesis)
+    return simplify(OR(results))
+
+
 
 # Here's an example of running the backward chainer - uncomment
 # it to see it work:
-#print backchain_to_goal_tree(ZOOKEEPER_RULES, 'opus is a penguin')
+# print backchain_to_goal_tree(ZOOKEEPER_RULES, 'opus is a penguin')
